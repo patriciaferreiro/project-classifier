e00d3ae3f1c3805ce50fa71d203a240e86f27300
patriciaferreiro <patricia.ferreiro@databricks.com>
2021-02-25 16:30:58 +0100

    add setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="project-classifier",
+    version="0.0.1",
+    author="Patricia Ferreiro",
+    author_email="patricia.ferreiro@databricks.com",
+    description="Project name classifier.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/patriciaferreiro/project-classifier",
+    project_urls={
+        "Bug Tracker": "https://github.com/patriciaferreiro/project-classifier/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+)
